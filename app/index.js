5b29ab8a0aba9e019e1facb43510a1e0e0b0868f
YardenDalii <yarden.dali11@gmail.com>
2024-07-02 19:36:02 +0300

    Firebase Database intergration

--- app/index.js
+++ app/index.js
@@ -1,4 +1,131 @@
-import React from 'react';
+// import React, { useState, useEffect } from 'react';
+// import { ScrollView } from 'react-native';
+// import LoginPage from './LoginPage';
+// import RegisterPage from './RegisterPage';
+// import HomePage from './HomePage';
+// import styles from '../stylesheets/LoginPageStyles';
+// import { auth, db } from '../firebase';
+// import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from 'firebase/auth';
+// import { setDoc, doc } from 'firebase/firestore';
+
+// export default App = () => {
+//   const [email, setEmail] = useState('');
+//   const [password, setPassword] = useState('');
+//   const [confirmPassword, setConfirmPassword] = useState('');
+//   const [firstName, setFirstName] = useState('');
+//   const [lastName, setLastName] = useState('');
+//   const [user, setUser] = useState(null); // Track user authentication state
+//   const [isLogin, setIsLogin] = useState(true); // State to toggle between login and register
+
+//   useEffect(() => {
+//     const unsubscribe = onAuthStateChanged(auth, (user) => {
+//       setUser(user);
+//     });
+
+//     return () => unsubscribe();
+//   }, [auth]);
+
+//   const handleLogin = async () => {
+//     try {
+//       await signInWithEmailAndPassword(auth, email, password);
+//       console.log('User signed in successfully!');
+//     } catch (error) {
+//       console.error('Login error:', error.message);
+//     }
+//   };
+
+//   const handleLogout = async () => {
+//     try {
+//       await signOut(auth);
+//       console.log('User signed out successfully!');
+//       setUser(null);
+//       switchToLogin();// Switch back to login page after logout
+//     } catch (error) {
+//       console.error('Logout error:', error.message);
+//     }
+//   };
+
+//   const handleRegister = async () => {
+//     if (password !== confirmPassword) {
+//       console.error('Passwords do not match!');
+//       return;
+//     }
+
+//     try {
+//       const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+//       const user = userCredential.user;
+
+//       await setDoc(doc(db, 'users', user.uid), {
+//         firstName,
+//         lastName,
+//         email: user.email,
+//       });
+
+//       console.log('User created and stored in Firestore successfully!');
+//       return switchToLogin;
+
+//     } catch (error) {
+//       console.error('Registration error:', error.message);
+//     }
+//   };
+
+//   const switchToLogin = () => {
+//     setIsLogin(true);
+//     setEmail('');
+//     setPassword('');
+//     setConfirmPassword('');
+//     setFirstName('');
+//     setLastName('');
+//   };
+
+//   const switchToRegister = () => {
+//     setIsLogin(false);
+//     setEmail('');
+//     setPassword('');
+//     setConfirmPassword('');
+//     setFirstName('');
+//     setLastName('');
+//   };
+
+//   return (
+//     <ScrollView contentContainerStyle={styles.container}>
+//       {user ? (
+//         <HomePage user={user} handleLogout={handleLogout} />
+//       ) : (
+//         isLogin ? (
+//           <LoginPage
+//             email={email}
+//             setEmail={setEmail}
+//             password={password}
+//             setPassword={setPassword}
+//             handleLogin={handleLogin}
+//             switchToRegister={switchToRegister}
+//           />
+//         ) : (
+//           <RegisterPage
+//             email={email}
+//             setEmail={setEmail}
+//             password={password}
+//             setPassword={setPassword}
+//             confirmPassword={confirmPassword}
+//             setConfirmPassword={setConfirmPassword}
+//             firstName={firstName}
+//             setFirstName={setFirstName}
+//             lastName={lastName}
+//             setLastName={setLastName}
+//             handleRegister={handleRegister}
+//             switchToLogin={switchToLogin}
+//           />
+//         )
+//       )}
+//     </ScrollView>
+//   );
+// };
+
+import React, { useState, useEffect } from 'react';
+import { ScrollView } from 'react-native';
+import LoginPage from './LoginPage';
+import RegisterPage from './RegisterPage';
 import HomePage from './HomePage';
 import styles from '../stylesheets/LoginPageStyles';
 import { auth, db } from '../firebase';
@@ -46,9 +173,9 @@ export default App = () => {
   };
 
   const handleRegister = async () => {
+    // TODO: add password regex
     if (password !== confirmPassword) {
       console.error('Passwords do not match!');
-      // TODO: add password regex
       return;
     }
 
@@ -61,10 +188,10 @@ export default App = () => {
         lastName,
         email: user.email,
       });
-      
+
       console.log('User created and stored in Firestore successfully!');
-      switchToLogin(); // Redirect to login page after successful signup
-      return (<LoginPage/>);
+      switchToLogin();
+      return; // Redirect to login page after successful signup
     } catch (error) {
       console.error('Registration error:', error.message);
     }
@@ -75,8 +202,8 @@ export default App = () => {
       await signOut(auth);
       console.log('User signed out successfully!');
       setUser(null);
-      switchToLogin(); // Switch back to login page after logout
-      return;
+      switchToLogin();
+      return (<LoginPage />); // Switch back to login page after logout
     } catch (error) {
       console.error('Logout error:', error.message);
     }
@@ -100,7 +227,37 @@ export default App = () => {
     setLastName('');
   };
 
-
-export default function App() {
-  return <HomePage />;
-}
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+      {user ? (
+        <HomePage user={user} fullName={fullName} handleLogout={handleLogout} />
+      ) : (
+        isLogin ? (
+          <LoginPage
+            email={email}
+            setEmail={setEmail}
+            password={password}
+            setPassword={setPassword}
+            handleLogin={handleLogin}
+            switchToRegister={switchToRegister}
+          />
+        ) : (
+          <RegisterPage
+            email={email}
+            setEmail={setEmail}
+            password={password}
+            setPassword={setPassword}
+            confirmPassword={confirmPassword}
+            setConfirmPassword={setConfirmPassword}
+            firstName={firstName}
+            setFirstName={setFirstName}
+            lastName={lastName}
+            setLastName={setLastName}
+            handleRegister={handleRegister}
+            switchToLogin={switchToLogin}
+          />
+        )
+      )}
+    </ScrollView>
+  );
+};
---
+++ firebase.js
@@ -0,0 +1,22 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCe6aWZMqrJqL1G8OIX2aFCeXtdK3K9ThE",
+    authDomain: "smartrecipe-e767f.firebaseapp.com",
+    projectId: "smartrecipe-e767f",
+    storageBucket: "smartrecipe-e767f.appspot.com",
+    messagingSenderId: "200937507860",
+    appId: "1:200937507860:web:58882ebbe053dbed0aab01",
+    measurementId: "G-BGJZL5CSNM"
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { db, auth }; 
