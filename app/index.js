46ba15a785494de25ff34ecd2fa3271936576aa9
Ester Moiseyev <esterm8297@gmail.com>
2024-07-12 11:27:39 +0300

    seperating the index.js from the authentication proccess by creating the AuthHandler.js file

--- app/index.js
+++ app/index.js
@@ -122,142 +122,15 @@
 //   );
 // };
 
-import React, { useState, useEffect } from 'react';
-import { ScrollView } from 'react-native';
-import LoginPage from './LoginPage';
-import RegisterPage from './RegisterPage';
-import HomePage from './HomePage';
-import styles from '../stylesheets/LoginPageStyles';
-import { auth, db } from '../firebase';
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from 'firebase/auth';
-import { setDoc, doc, getDoc } from 'firebase/firestore';
-import { Route } from 'expo-router/build/Route';
-import { router } from 'expo-router';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import Layout from './_layout';
+import AuthHandler from '../utils/AuthHandler';
 
-export default App = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [user, setUser] = useState(null); // Track user authentication state
-  const [fullName, setFullName] = useState(''); // Store full name
-  const [isLogin, setIsLogin] = useState(true); // State to toggle between login and register
-
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, async (user) => {
-      if (user) {
-        const docRef = doc(db, 'users', user.uid);
-        const docSnap = await getDoc(docRef);
-
-        if (docSnap.exists()) {
-          const userData = docSnap.data();
-          setFullName(`${userData.firstName} ${userData.lastName}`);
-        }
-        setUser(user);
-      } else {
-        setUser(null);
-      }
-    });
-
-    return () => unsubscribe();
-  }, [auth]);
-
-  const handleLogin = async () => {
-    try {
-      await signInWithEmailAndPassword(auth, email, password);
-      console.log('User signed in successfully!');
-    } catch (error) {
-      console.error('Login error:', error.message);
-    }
-  };
-
-  const handleRegister = async () => {
-    // TODO: add password regex
-    if (password !== confirmPassword) {
-      console.error('Passwords do not match!');
-      return;
-    }
-
-    try {
-      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-      const user = userCredential.user;
-
-      await setDoc(doc(db, 'users', user.uid), {
-        firstName,
-        lastName,
-        email: user.email,
-      });
-
-      console.log('User created and stored in Firestore successfully!');
-      switchToLogin();
-      return; // Redirect to login page after successful signup
-    } catch (error) {
-      console.error('Registration error:', error.message);
-    }
-  };
-
-  const handleLogout = async () => {
-    try {
-      await signOut(auth);
-      console.log('User signed out successfully!');
-      setUser(null);
-      switchToLogin();
-      return (<LoginPage />); // Switch back to login page after logout
-    } catch (error) {
-      console.error('Logout error:', error.message);
-    }
-  };
-
-  const switchToLogin = () => {
-    setIsLogin(true);
-    setEmail('');
-    setPassword('');
-    setConfirmPassword('');
-    setFirstName('');
-    setLastName('');
-  };
-
-  const switchToRegister = () => {
-    setIsLogin(false);
-    setEmail('');
-    setPassword('');
-    setConfirmPassword('');
-    setFirstName('');
-    setLastName('');
-  };
-
+const App = () => {
   return (
-    <ScrollView contentContainerStyle={styles.container}>
-      {user ? (
-        <HomePage user={user} fullName={fullName} handleLogout={handleLogout} />
-      ) : (
-        isLogin ? (
-          <LoginPage
-            email={email}
-            setEmail={setEmail}
-            password={password}
-            setPassword={setPassword}
-            handleLogin={handleLogin}
-            switchToRegister={switchToRegister}
-          />
-        ) : (
-          <RegisterPage
-            email={email}
-            setEmail={setEmail}
-            password={password}
-            setPassword={setPassword}
-            confirmPassword={confirmPassword}
-            setConfirmPassword={setConfirmPassword}
-            firstName={firstName}
-            setFirstName={setFirstName}
-            lastName={lastName}
-            setLastName={setLastName}
-            handleRegister={handleRegister}
-            switchToLogin={switchToLogin}
-          />
-        )
-      )}
-    </ScrollView>
+      <AuthHandler />
   );
 };
+
+export default App;
