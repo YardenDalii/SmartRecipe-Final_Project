562e6438f79b249dbd7f14cf2146acae4fe336be
Ester Moiseyev <esterm8297@gmail.com>
2024-07-19 10:17:52 +0300

    adding change password button

--- app/ProfilePage.js
+++ app/ProfilePage.js
@@ -1,18 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, TextInput, TouchableOpacity, ActivityIndicator, Alert } from 'react-native';
 import profilePageStyles from '../stylesheets/ProfilePageStyles'; // Import the profile page styles
-import { useNavigation } from '@react-navigation/native'; 
-import { db, auth } from '../firebase'; // Import Firebase auth and db
-import { doc, getDoc, updateDoc, deleteDoc  } from 'firebase/firestore';
-import { deleteUser } from 'firebase/auth';
+import { useNavigation } from '@react-navigation/native';
+import { db, auth } from '../firebase'; // Import Firebase db and auth
+import { doc, getDoc, updateDoc, deleteDoc } from 'firebase/firestore';
+import { deleteUser, reauthenticateWithCredential, EmailAuthProvider, updatePassword } from 'firebase/auth';
 
 const ProfilePage = ({ user, onClose }) => {
   const navigation = useNavigation();
-  const [fullName, setFullName] = useState('User USERS');
-  const [email, setEmail] = useState('email@email.com');
-  const [phoneNumber, setPhoneNumber] = useState('0501111111');
+  const [fullName, setFullName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [oldPassword, setOldPassword] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+  const [confirmNewPassword, setConfirmNewPassword] = useState('');
   const [isEditing, setIsEditing] = useState(false);
   const [loading, setLoading] = useState(true);
+  const [showPasswordFields, setShowPasswordFields] = useState(false); // State to toggle password fields
 
   useEffect(() => {
     const fetchUserData = async () => {
@@ -37,10 +41,6 @@ const ProfilePage = ({ user, onClose }) => {
     fetchUserData();
   }, [user]);
 
-  const handleEditPress = () => {
-    setIsEditing(true);
-  };
-
   const handleSavePress = async () => {
     try {
       const [firstName, lastName] = fullName.split(' ');
@@ -48,7 +48,6 @@ const ProfilePage = ({ user, onClose }) => {
       await updateDoc(docRef, {
         firstName,
         lastName,
-        email,
         phoneNumber,
       });
       setIsEditing(false);
@@ -56,6 +55,16 @@ const ProfilePage = ({ user, onClose }) => {
     } catch (error) {
       console.error('Error updating user data:', error);
     }
+  };
+
+  const handleFavoriteRecipesPress = () => {
+    console.log('Favorite Recipes Button Pressed');
+    // Add the navigation logic or actions here
+  };
+
+  const handleMyRecipesPress = () => {
+    console.log('My Recipes Button Pressed');
+    navigation.navigate('MyRecipesPage'); // Navigate to MyRecipesPage
   };
 
   const handleDeleteAccount = async () => {
@@ -85,19 +94,39 @@ const ProfilePage = ({ user, onClose }) => {
     );
   };
 
+  const handleChangePassword = async () => {
+    if (newPassword !== confirmNewPassword) {
+      Alert.alert('Error', 'New passwords do not match.');
+      return;
+    }
+
+    const credential = EmailAuthProvider.credential(user.email, oldPassword);
+
+    try {
+      await reauthenticateWithCredential(user, credential);
+      await updatePassword(user, newPassword);
+      Alert.alert('Success', 'Password changed successfully.');
+      setOldPassword('');
+      setNewPassword('');
+      setConfirmNewPassword('');
+      setShowPasswordFields(false); // Hide password fields after successful change
+    } catch (error) {
+      let errorMessage = 'Error changing password.';
+      if (error.code === 'auth/wrong-password') {
+        errorMessage = 'The old password you entered is incorrect.';
+      } else if (error.code === 'auth/weak-password') {
+        errorMessage = 'The new password is too weak.';
+      } else if (error.code === 'auth/invalid-credential') {
+        errorMessage = 'Invalid credentials. Please try again.';
+      }
+      Alert.alert('Error', errorMessage);
+      console.error('Error changing password:', error);
+    }
+  };
+
   if (loading) {
     return <ActivityIndicator size="large" color="#0000ff" />;
   }
-
-  const handleFavoriteRecipesPress = () => {
-    console.log('Favorite Recipes Button Pressed');
-    // Add the navigation logic or actions here
-  };
-
-  const handleMyRecipesPress = () => {
-    console.log('My Recipes Button Pressed');
-    navigation.navigate('MyRecipesPage'); // Navigate to MyRecipesPage
-  };
 
   return (
     <View style={profilePageStyles.container}>
@@ -136,7 +165,6 @@ const ProfilePage = ({ user, onClose }) => {
           <Text style={profilePageStyles.buttonText}>Edit</Text>
         </TouchableOpacity>
       )}
-
       <TouchableOpacity style={profilePageStyles.button} onPress={handleFavoriteRecipesPress}>
         <Text style={profilePageStyles.buttonText}>Favorite Recipes</Text>
       </TouchableOpacity>
@@ -144,7 +172,48 @@ const ProfilePage = ({ user, onClose }) => {
         <Text style={profilePageStyles.buttonText}>My Recipes</Text>
       </TouchableOpacity>
 
-      <TouchableOpacity style={[profilePageStyles.button, profilePageStyles.deleteButton]} onPress={confirmDeleteAccount}>
+      {/* Button to Show/Hide Password Fields */}
+      <TouchableOpacity
+        style={profilePageStyles.button}
+        onPress={() => setShowPasswordFields(!showPasswordFields)}
+      >
+        <Text style={profilePageStyles.buttonText}>Change Password</Text>
+      </TouchableOpacity>
+
+      {/* Change Password Fields */}
+      {showPasswordFields && (
+        <View style={profilePageStyles.changePasswordContainer}>
+          <TextInput
+            style={profilePageStyles.input}
+            placeholder="Old Password"
+            value={oldPassword}
+            onChangeText={setOldPassword}
+            secureTextEntry
+          />
+          <TextInput
+            style={profilePageStyles.input}
+            placeholder="New Password"
+            value={newPassword}
+            onChangeText={setNewPassword}
+            secureTextEntry
+          />
+          <TextInput
+            style={profilePageStyles.input}
+            placeholder="Confirm New Password"
+            value={confirmNewPassword}
+            onChangeText={setConfirmNewPassword}
+            secureTextEntry
+          />
+          <TouchableOpacity style={profilePageStyles.button} onPress={handleChangePassword}>
+            <Text style={profilePageStyles.buttonText}>Save New Password</Text>
+          </TouchableOpacity>
+        </View>
+      )}
+
+      <TouchableOpacity
+        style={[profilePageStyles.button, profilePageStyles.deleteButton]}
+        onPress={confirmDeleteAccount}
+      >
         <Text style={profilePageStyles.buttonText}>Delete Account</Text>
       </TouchableOpacity>
     </View>
