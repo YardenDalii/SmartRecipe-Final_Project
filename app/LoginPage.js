584c0c43ac4fb55ba568e36238c9dba0d8789369
YardenDalii <yarden.dali11@gmail.com>
2024-08-21 19:24:22 +0300

    loginpage fixing

--- app/LoginPage.js
+++ app/LoginPage.js
@@ -116,14 +116,14 @@ const LoginPage = ({ email, setEmail, password, setPassword, handleLogin, switch
                     onChangeText={setPassword}
                     secureTextEntry
                 />
+                <View style={styles.toggleTextContainer}>
+                        <Text style={styles.toggleText} onPress={() => switchToReset(navigation)}>
+                            Forgot your password?</Text>
+                </View>
                 <View style={styles.buttonContainer}>
                     <TouchableOpacity style={styles.button} onPress={() => handleLogin(navigation)}>
                         <Text style={styles.buttonText}>Sign In</Text>
                     </TouchableOpacity>
-                </View>
-                <View style={styles.toggleTextContainer}>
-                        <Text style={styles.toggleText} onPress={() => switchToReset(navigation)}>
-                            Forgot your password?</Text>
                 </View>
                 <View style={styles.buttonContainer}>
                     <TouchableOpacity style={styles.button} onPress={continueWithoutLogin}>
@@ -134,11 +134,6 @@ const LoginPage = ({ email, setEmail, password, setPassword, handleLogin, switch
                     <Text style={styles.toggleText} onPress={() => switchToRegister(navigation)}>
                         Need an account? Sign Up
                     </Text>
-                </View>
-                <View style={styles.buttonContainer}>
-                    <TouchableOpacity style={styles.button} onPress={continueWithoutLogin}>
-                        <Text style={styles.buttonText}>Continue without Login</Text>
-                    </TouchableOpacity>
                 </View>
                 <View style={styles.buttonContainer}>
                     <TouchableOpacity
