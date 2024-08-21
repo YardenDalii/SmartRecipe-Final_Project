16545724df4104d846aec0c398c4a65abf6d0070
Ester Moiseyev <esterm8297@gmail.com>
2024-08-21 19:05:13 +0300

    aboutUsPage connection

---
+++ app/AboutUsPage.js
@@ -0,0 +1,15 @@
+import { View, Text, Button, TouchableOpacity } from 'react-native';
+
+const AboutUsPage = () => {
+
+    return (
+        <View>
+            <Text>Hellooo</Text>
+        </View>
+    );
+};
+
+export default AboutUsPage;
+
+
+
